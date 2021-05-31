86a49dfe55f6b8c45795fd930310d85afaaec87f
stephanie-vm <stephaniecetav@gmail.com>
2021-05-31 16:36:34 -0600

    Agrego de nuevo los cambios que no se actualizaron

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d'); 
+const ctx = canvas.getContext('2d');
+const square = {
+    xPosition: 0,
+    yPosition: 0,
+    xWidth: 0,
+    yHeight: 0,
+    R: 0,
+    G: 0,
+    B: 0,
+    A: .8
+};
 
-ctx.font = '50px serif';
-ctx.fillText('Stephanie', 150, 170);
-
-function draw() {
-    
-    window.requestAnimationFrame(draw);
+function newRandom(min, max) {
+  return Math.floor(Math.random() * (min - max) + max);
 }
 
-window.requestAnimationFrame(draw);
+function draw() {
+ctx.fillStyle = 'rgba(' + square.R + ',' + square.G + ',' + square.B + ',' + square.A +')';
+ctx.fillRect(square.xPosition,square.yPosition,square.xWidth,square.yHeight);
+}
+
+function update() {
+    square.xPosition = newRandom(0, canvas.width);
+    square.yPosition = newRandom(0, canvas.height);
+    square.xWidth = newRandom(-15, 15);
+    square.yHeight = newRandom(-15, 15);
+    square.R = newRandom(0,200);
+    square.G = newRandom(0,200);
+    square.B = newRandom(0,200);
+}
+
+function animationSquare() {
+    update();
+    draw();
+    window.requestAnimationFrame(animationSquare);
+}
+
+window.requestAnimationFrame(animationSquare)
